a866540aabfa49b19e99c485a1adf5bd5b1f2c1f
Virginia Rabello <virginia.rabello2107@gmail.com>
2021-07-23 15:46:14 -0500

    deploy to heroku

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/budget-tracker', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
